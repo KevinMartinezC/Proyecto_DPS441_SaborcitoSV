27e0bdaa5d50ef59f06b4bcb410a6b43384d5eac
AdonayMejia <adonay.mejia021@gmail.com>
2022-03-20 12:11:35 -0600

    Mandando a rama stanley

---
+++ App.js
@@ -0,0 +1,10 @@
+import React,{useEffect} from 'react';
+
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Navigation from "./app/Navigations/Navigation";
+
+export default function App() {
+
+  return <Navigation/>;
+}
